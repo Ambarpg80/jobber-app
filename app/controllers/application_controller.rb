1bf6d7a222783d367eba39f81937a09c0934183b
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-09-02 11:00:59 -0400

    add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
     include ActionController::Cookies 
     before_action :authorized
-    rescue_from ActiveRecord::RecordInvalid, with: :render_not_authorized_error
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
@@ -9,16 +8,12 @@ class ApplicationController < ActionController::Base
 
     private
     def current_user
-        current_user = User.find_by(id: session[:user_id])
+       User.find_by(id: session[:user_id])
     end 
 
     def authorized
-        return render json: { error: "Not Authorized" }, status: :unauthorized unless @current_user
-    end
-
-    def render_not_authorized
         user = current_user
-        render json: {errors: [user.errors.full_messages]}, status: :unauthorized
+        return render json: { error: "Not Authorized. Please Login or Sign up" }, status: :unauthorized unless user
     end
 
     def render_unprocessable_entity
