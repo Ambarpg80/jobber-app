89962d732c237576fdb26f9c737ec956341936b8
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-09-08 04:05:19 -0400

    remove dependent destroy option

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    has_many :inquiries , dependent: :destroy
+     has_many :inquiries 
     has_many :users, through: :inquiries
 
     validates :company_name, :title, :location, :description, presence: true
