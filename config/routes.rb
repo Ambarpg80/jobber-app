f5bb0981e1525595bfbd304a02bb286331cab4a6
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-09-08 04:01:32 -0400

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "posts#index"                      
+                   
   get "/posts", to: "posts#index"         #shows all the posts
-  post "/posts", to: "posts#create"       #creates a job post
-  get "/show_post", to: "posts#show"      #gets a single post
-  patch "/posts/:id", to: "posts#update"
-  delete "/posts/:id", to: "posts#destroy"
-
-  # get "/users", to: "users#index"         #gets all the users
+  
+  patch "/users/:id", to: "users#update" #update user info
   get "/me", to: "users#show"          #automatically signs user in
   post "/signup", to: "users#create"    #creates a user account
   
   post "/login", to: "sessions#create"   #creates a session- logs user in
   delete "/logout", to: "sessions#destroy" #deletes session_id - logs user out
   
-  get "/posts/login", to: "inquiries#login_to_apply"
-  get "/inquiries", to: "inquiries#index"               #shows all inquiries(job_applications)
   post "/inquiries", to: "inquiries#create"             #create inquiry(job_application)
-  delete "/me/inquiries/:id", to: "inquiries#destroy"     #delete inquiry belonging to user(withdraws application) 
+  delete "/inquiries/:id", to: "inquiries#destroy"     #delete inquiry belonging to user(withdraws application) 
   
 
 end
