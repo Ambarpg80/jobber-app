772d53f20710ed6309eb487911db290004c459cc
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-09-14 15:05:59 -0400

    update routes to use resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-                   
-  get "/posts", to: "posts#index"         #shows all the posts
   
-  patch "/users/:id", to: "users#update" #update user info
+  resources :posts, only: :index         #shows all the posts       
+  resources :users, only: :update  #update user info
+  resources :inquiries, only: [:create, :destroy]
+
   get "/me", to: "users#show"          #automatically signs user in
   post "/signup", to: "users#create"    #creates a user account
   
   post "/login", to: "sessions#create"   #creates a session- logs user in
   delete "/logout", to: "sessions#destroy" #deletes session_id - logs user out
   
-  post "/inquiries", to: "inquiries#create"             #create inquiry(job_application)
-  delete "/inquiries/:id", to: "inquiries#destroy"     #delete inquiry belonging to user(withdraws application) 
+
+ 
+  
   
 
 end
